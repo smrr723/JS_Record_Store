fe636321a4dd761ee134c6cbd48f2c8fac0fbb07
Scott Murray <rai.five@gmail.com>
2017-12-02 19:50:48 +0000

    Task Specs

--- record.js
+++ record.js
@@ -1,17 +1,2 @@
-var assert = require("assert");
-var Record = require("../record");
 
-describe("Record", function(){
-
-   beforeEach("Setup", function(){
-      record = new Record("Lenny Kravitz", "Fly Away", "Rock", 10);
-   });
-
-   it("should have properties", function(){
-      assert.strictEqual(record.artist, "Lenny Kravitz");
-      assert.strictEqual(record.title, "Fly Away");
-      assert.strictEqual(record.genre, "Rock");
-      assert.strictEqual(record.price, 10);
-   });
-
-});
+module.exports = Record;
