b9a654b7c5426f3306be380795902b1ac2d80f24
Scott Murray <rai.five@gmail.com>
2017-12-02 20:00:59 +0000

    Record Store Spec addded

---
+++ specs/record_store_spec.js
@@ -0,0 +1,22 @@
+var assert = require("assert");
+var Record = require("../record");
+var Record_Store = require("../record_store");
+
+describe("Record Store", function(){
+   beforeEach("Setup", function(){
+      record_store = new Record_Store("CodeClan Records", "Glasgow");
+      record1 = new Record("Lenny Kravitz", "Fly Away", "Rock", 10);
+      record2 = new Record("Daft Punk", "Harder Better Faster Stronger", "Electronic", 12);
+      record3 = new Record("ACDC", "Rock and Roll Train", "Rock", 8);
+      record_store.add();
+   });
+   it("should have properties", function(){
+      assert.strictEqual(record_store.artist, "Lenny Kravitz");
+   });
+   it("should be able to hold records", function(){
+      assert.strictEqual(record_store.inventory[0].artist, "Lenny Kravitz");
+   });
+   it("should have a balance", function(){
+      assert.strictEqual(record_store.balance, 1000);
+   })
+});
